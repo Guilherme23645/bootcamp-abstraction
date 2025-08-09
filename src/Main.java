6c1e27f876492640a6671c3fb983495722ab66c1
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-09 19:08:31 -0300

    feat: display features

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import br.com.dio.desafio.dominio.Course;
+import br.com.dio.desafio.dominio.Mentorship;
+import java.time.LocalDate;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Course course1 = new Course();
+		course1.setTitle("Java");
+		course1.setDescription("This is a Java Course");
+		course1.setHourLoad(100);
+
+		Course course2 = new Course();
+		course2.setTitle("Python");
+		course2.setDescription("This is a Python Course");
+		course2.setHourLoad(75);
+
+		Mentorship mentorship = new Mentorship();
+		mentorship.setTitle("JS Mentorship");
+		mentorship.setDescription("This is a JS Mentorship");
+		mentorship.setDate(LocalDate.now());
+
+		System.out.println(course1);
+		System.out.println(course2);
+		System.out.println(mentorship);
 	}
 }
