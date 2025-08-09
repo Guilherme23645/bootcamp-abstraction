5be35ba7bff8ed82d9f224d660af8763b02f764a
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-09 20:48:41 -0300

    feat: add methods and attributes to Bootcamp

--- src/br.com.dio.desafio.dominio/Bootcamp.java
+++ src/br.com.dio.desafio.dominio/Bootcamp.java
@@ -1,5 +1,71 @@
 package br.com.dio.desafio.dominio;
 
+import java.time.LocalDate;
+
 public class Bootcamp {
-	
+	private String name;
+	private String description;
+	private final LocalDate initialDate = LocalDate.now();
+	private final LocalDate finalDate = initialDate.plusDays(45);
+	private Set<Dev> enrolledDevs = new HashSet<>();
+	private Set<Content> contents = new LinkedHashSet<>();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getInitialDate() {
+		return initialDate;
+	}
+
+	public LocalDate getFinalDate() {
+		return finalDate;
+	}
+
+	public Set<Dev> getEnrolledDevs() {
+		return enrolledDevs;
+	}
+
+	public void setEnrolledDevs(Set<Dev> enrolledDevs) {
+		this.enrolledDevs = enrolledDevs;
+	}
+
+	public Set<Content> getContents() {
+		return contents;
+	}
+
+	public void setContents(Set<Content> contents) {
+		this.contents = contents;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Bootcamp bootcamp = (Bootcamp) o;
+		return 
+		Objects.equals(name, bootcamp.name) &&
+		Objects.equals(description, bootcamp.description) &&
+		Objects.equals(initialDate, bootcamp.initialDate) &&
+		Objects.equals(finalDate, bootcamp.finalDate) &&
+		Objects.equals(enrolledDevs, bootcamp.enrolledDevs) &&
+		Objects.equals(contents, bootcamp.contents);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name,description,initialDate,finalDate,enrolledDevs,contents);
+	}
 }
