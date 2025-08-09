7a086167d1c06b70b10efd1d8a93ac51e0f98224
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-09 19:23:06 -0300

    feat: define methods and attributes

--- src/br.com.dio.desafio.dominio/Course.java
+++ src/br.com.dio.desafio.dominio/Course.java
@@ -4,4 +4,41 @@ public class Course {
 	private String title;
 	private String description;
 	private int hourLoad;
+
+	public Course() {
+
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getHourLoad() {
+		return hourLoad;
+	}
+
+	public void setHourLoad(int hourLoad) {
+		this.hourLoad = hourLoad;
+	}
+
+	@Override
+	public String toString() {
+		return "Course{"+
+			"title='"+title+'\''+
+			", description='"+description+'\''+
+			", hourLoad='"+hourLoad+'\''+
+			'}';
+	}
 }
