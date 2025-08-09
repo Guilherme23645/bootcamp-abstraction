974a514edd2e461d29e431f9ca5ed277bf93485b
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-09 20:49:08 -0300

    feat: add methods and attributes to Dev

--- src/br.com.dio.desafio.dominio/Dev.java
+++ src/br.com.dio.desafio.dominio/Dev.java
@@ -1,5 +1,77 @@
 package br.com.dio.desafio.dominio;
 
-public class Dev {
+import java.util.Set;
+import java.util.LinkedHashSet;
 
+public class Dev {
+	private String name;
+	private Set<Content> enrolledContents = new LinkedHashSet<>();
+	private Set<Content> finishedContents = new LinkedHashSet<>();
+	
+	public void enrollBootcamp(Bootcamp bootcamp) {
+		
+	}
+
+	public void progress() {
+	
+	}
+
+	public void calcXp() {
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Content> getEnrolledContents() {
+		return enrolledContents;
+	}
+
+	public void setEnrolledContents(Set<Content> enrolledContents) {
+		this.enrolledContents = enrolledContents;
+	}
+
+	public Set<Content> getFinishedContents() {
+		return finishedContents;
+	}
+
+	public void setFinishedContents(Set<Content> finishedContents) {
+		this.finishedContents = finishedContents;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Dev dev = (Dev) o;
+		return Objects.equals(name, dev.name) && Objects.equals(enrolledContents, dev.enrolledContents) && Objects.equals(finishedContents, dev.finishedContents);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, enrolledContents, finishedContents);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
